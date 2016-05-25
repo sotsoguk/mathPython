01ef2f97eea8898494200b80c6bd03d7e743a845
sotsoguk <sotsoguk@gmail.com>
2016-05-25 12:36:54 +0200

    Color of circle

--- 06-drawing/circle.py
+++ 06-drawing/circle.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 
 def create_circle():
-	circle = plt.Circle( (0,0), 0.05)
+	circle = plt.Circle( (0,0), 0.05, fc='red', ec='green')
 	return circle
 
 def update_radius(i, circle):
